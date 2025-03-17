23cdce2b3a5e5f459a0e6d9917c5c5f57ae73a95
Chkarte <iftm03@hotmail.com>
2025-03-17 20:47:43 -0300

    Portafolio Profesional de Javier Lopez

---
+++ script.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+
+
+/* Open when someone clicks on the span element */
+
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+  document.getElementById("myNav1").style.height = "100%";
+
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+  document.getElementById("myNav").style.height = "0%";
+}
+
+
+
